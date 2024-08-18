f1a56b8dc596bb32376e61cb987b6012e632778b
krunal21041993 <kathikar.krunal3@gmail.com>
2024-08-18 15:40:08 +0530

    Addition is implementation in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    Calculator calculator = new Calculator();
-	    //calculator.getResult()
+        System.out.println("Addition is  : "+calculator.getResult(new Addition(1,2)));
     }
 }
